98723e49626cbd132ca198a5524a1b6925c7f42d
tkhang <haphongpk12@gmail.com>
2019-09-21 18:02:57 +0700

    Done WP2

--- spriteutil.py
+++ spriteutil.py
@@ -1,6 +1,7 @@
 import timeit
 import operator
 import numpy as np
+from collections import defaultdict
 from PIL import Image
 
 
@@ -14,20 +15,53 @@ def find_most_common_color(image):
         - a tuple (red, green, blue) of integers if the mode is RGB;
         - a tuple (red, green, blue, alpha) of integers if the mode is RGBA.
     """
-    width, height = image.size
-    color_count = {}
-    for w in range(width):
-        for h in range(height):
-            current_color = image.getpixel((w, h))
-            if current_color in color_count:
-                color_count[current_color] += 1
-            else:
-                color_count[current_color] = 1
+    color_count = defaultdict(int)
+    for pixel in image.getdata():
+        color_count[pixel] += 1
     return max(color_count.items(), key=operator.itemgetter(1))[0]
 
 
+class Sprite:
+    def __init__(self, label, x1, y1, x2, y2):
+        check_list = [label, x1, y1, x2, y2]
+        for elm in check_list:
+            if isinstance(elm, str) or elm < 0 or (x1, y1) > (x2, y2):
+                raise ValueError("Invalid coordinates")
+            else:
+                self._label = label
+                self._top_left = (x1, y1)
+                self._bottom_right = (x2, y2)
+
+                self.x1 = x1
+                self.y1 = y1
+                self.x2 = x2
+                self.y2 = y2
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def top_left(self):
+        return (self.x1, self.y1)
+
+    @property
+    def bottom_right(self):
+        return (self.x2, self.y2)
+
+    @property
+    def width(self):
+        return self.x2 - self.x1 + 1
+
+    @property
+    def height(self):
+        return self.y2 - self.y1 + 1
+
+
 if __name__ == "__main__":
-    image = Image.open(
-        "./resources/islands.png")
-    image = image.convert("L")
-    print(find_most_common_color(image))
+    # image = Image.open(
+    #     "./resources/islands.png")
+    # image = image.convert("L")
+    # print(find_most_common_color(image))
+    sprite = Sprite(1, 12, 23, 145, 208)
+    print(sprite.height)
